22f5ba1072f0f7cc49d3b0546bfd6eb37caa3f43
Kareem Skinner <kareem@karski.io>
2021-05-18 11:09:57 -0400

    First triangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 // GLEW loads OpenGL function pointers from the system's graphics drivers.
+// glew.h MUST be included before gl.h
 #include <GL/glew.h>
+#include <GL/gl.h>
 
 // Disables inclusion of the dev-environ header.
 // Allows GLFW + extension loader headers to be included in any order.
@@ -22,9 +24,9 @@ int main()
         exit(EXIT_FAILURE);
     }
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    //glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     GLFWwindow* window = glfwCreateWindow(initialWindowWidth, initialWindowHeight, "Oglre", NULL, NULL);
     if (!window) {
@@ -35,15 +37,33 @@ int main()
     }
 
     glfwMakeContextCurrent(window);
+
+    // A valid OpenGL context must be created before initializing GLEW.
     bool error = glewInit();
     if (error != GLEW_OK) {
         std::cout << "Error: Failed to initialize OpenGL function pointer loader!\n";
     }
     // Initialize OpenGL loader (GLEW in this project)
 
+    // Printing OpenGL version for convenience
+    std::cout << "OpenGL Version + System GPU Drivers: " << glGetString(GL_VERSION) << std::endl;
+
     // Main loop
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Triangle just to test library
+        glBegin(GL_TRIANGLES);
+
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+
+        glEnd();
+        // Swaps the front and back buffers of the specified window.
+        glfwSwapBuffers(window);
     }
 
     glfwDestroyWindow(window);
