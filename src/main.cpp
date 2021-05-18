6512402e6e765977ba52ad66799a65f263fb9706
Kareem Skinner <kareem@karski.io>
2021-05-18 11:21:54 -0400

    First triangle

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 
+#include <vector>
 #include <iostream>
 
 const int initialWindowWidth = 800;
@@ -28,46 +29,56 @@ int main()
     //glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
     //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(initialWindowWidth, initialWindowHeight, "Oglre", NULL, NULL);
-    if (!window) {
-        std::cout << "GLFW Window creation failed\n"
-                  << "Exiting...\n";
+    GLFWwindow* glfwMakeContextCurrent(window);
+    << "GLFW Window creation failed\n"
+    << "Exiting...\n";
 
-        exit(EXIT_FAILURE);
-    }
+    exit(EXIT_FAILURE);
+}
 
-    glfwMakeContextCurrent(window);
+// A valid OpenGL context must be created before initializing GLEW.
+bool error = glewInit();
+if (error != GLEW_OK) {
+    std::cout << "Error: Failed to initialize OpenGL function pointer loader!\n";
+}
+// Initialize OpenGL loader (GLEW in this project)
 
-    // A valid OpenGL context must be created before initializing GLEW.
-    bool error = glewInit();
-    if (error != GLEW_OK) {
-        std::cout << "Error: Failed to initialize OpenGL function pointer loader!\n";
-    }
-    // Initialize OpenGL loader (GLEW in this project)
+// Printing OpenGL version for convenience
+std::cout << "OpenGL Version + System GPU Drivers: " << glGetString(GL_VERSION) << std::endl;
 
-    // Printing OpenGL version for convenience
-    std::cout << "OpenGL Version + System GPU Drivers: " << glGetString(GL_VERSION) << std::endl;
+std::vector<float> trianglePositions = {
+    -0.5f, -0.5,
+    0.0f, 0.5f,
+    0.5f, -0.5f
+};
+// Main buffer
+unsigned int buffer
+    = 0;
+glGenBuffers(1, &buffer);
+glBindBuffer(GL_ARRAY_BUFFER, buffer);
+glBufferData(GL_ARRAY_BUFFER, )
 
     // Main loop
-    while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
+    while (!glfwWindowShouldClose(window))
+{
+    glfwPollEvents();
 
-        glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT);
 
-        // Triangle just to test library
-        glBegin(GL_TRIANGLES);
+    // Triangle just to test library
+    glBegin(GL_TRIANGLES);
 
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
+    glVertex2f(-0.5f, -0.5f);
+    glVertex2f(0.0f, 0.5f);
+    glVertex2f(0.5f, -0.5f);
 
-        glEnd();
-        // Swaps the front and back buffers of the specified window.
-        glfwSwapBuffers(window);
-    }
+    glEnd();
+    // Swaps the front and back buffers of the specified window.
+    glfwSwapBuffers(window);
+}
 
-    glfwDestroyWindow(window);
-    glfwTerminate();
+glfwDestroyWindow(window);
+glfwTerminate();
 
-    exit(EXIT_SUCCESS);
+exit(EXIT_SUCCESS);
 }
